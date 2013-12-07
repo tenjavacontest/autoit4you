f87173e90881e27f076cb3337ca58c2df3cd9e0d
autoit4you <gutenbergschule.horn@gmail.com>
2013-12-07 16:00:26 +0100

    Created basic skeleton

---
+++ src/main/java/de/autoit4you/tenjava/TenJava.java
@@ -0,0 +1,23 @@
+package de.autoit4you.tenjava;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Project: tenjava
+ * Class: tenjava
+ * Version: 1.0
+ * Date: 30.11.13
+ * Copyright (c) 2013 by Jonathan Horn
+ */
+public class TenJava extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
